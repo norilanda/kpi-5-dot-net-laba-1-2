c2c09708c86298e764738a09450407f5da6c3979
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-10-13 20:19:03 +0300

    Add enumerators tests

---
+++ MyBinaryTree.Tests/EnumeratorsTests/EnumeratorFactoriesTests.cs
@@ -0,0 +1,62 @@
+﻿using MyBinaryTree.EnumeratorFactories;
+using MyBinaryTree.Interfaces;
+using System.Collections;
+using Xunit;
+
+namespace MyBinaryTree.Tests.EnumeratorsTests;
+
+public class EnumeratorFactoriesTests
+{
+    private static readonly int[] _initialData = { 4, 3, 1, 5, 2 };
+    public static IEnumerable<object[]> GetEnumeratorFactories()
+    {
+        yield return new object[] { new InorderEnumeratorFactory<int>() };
+        yield return new object[] { new PreorderEnumeratorFactory<int>() };
+        yield return new object[] { new PostorderEnumeratorFactory<int>() };
+    }
+
+    public static IEnumerable<object[]> GetEnumeratorFactoriesAndResultSequence()
+    {
+        yield return new object[] { new InorderEnumeratorFactory<int>(), _initialData, new int[] { 1, 2, 3, 4, 5 } };
+        yield return new object[] { new PreorderEnumeratorFactory<int>(), _initialData, new int[] { 4, 3, 1, 2, 5 } };
+        yield return new object[] { new PostorderEnumeratorFactory<int>(), _initialData, new int[] { 2, 1, 3, 5, 4 } };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetEnumeratorFactories))]
+    public void MoveNext_WhenTreeIsNotEmpty_ShouldBeTrueWhileNodesAvailable(IEnumeratorFactory<int> factory)
+    {
+        var tree = new BinaryTree<int>(factory) { 3, 1, 2 };
+        int nodesNumber = 3;
+
+        var enumerator = tree.GetEnumerator();
+
+        for (int i = 0; i < nodesNumber; i++)
+        {
+            Assert.True(enumerator.MoveNext());
+        }
+        Assert.False(enumerator.MoveNext());
+    }
+
+    [Theory]
+    [MemberData(nameof (GetEnumeratorFactoriesAndResultSequence))]
+    public void CreateEnumerator_WhenTreeIsNotEmpty_ShouldReturnSpecifiedSequence(
+        IEnumeratorFactory<int> factory, 
+        int[] items, 
+        int[] expectedSequence)
+    {
+        // arrange
+        var tree = new BinaryTree<int>(factory, items);
+
+        // act
+        var enumerator = tree.GetEnumerator();
+
+        // assert
+        foreach (var item in expectedSequence)
+        {
+            enumerator.MoveNext();
+            Assert.Equal(item, enumerator.Current);
+            Assert.Equal(item, ((IEnumerator)enumerator).Current);
+        }
+    }
+}
---
+++ MyBinaryTree.Tests/EnumeratorsTests/InorderEnumeratorTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace MyBinaryTree.Tests.EnumeratorsTests;
+
+public class InorderEnumeratorTests
+{
+    [Fact]
+    public void Reset_WhenTreeWasUnchanged_ShouldReset()
+    {
+        // arrange
+        var tree = new BinaryTree<int>() { 2, 5, 3 };
+        var root = tree.Root;
+        var enumerator = tree.GetEnumerator();
+
+        while (enumerator.MoveNext()) { }
+
+        // act
+        enumerator.Reset();
+
+        // assert
+        Assert.Equal(root!.Value, enumerator.Current);
+    }
+
+    [Fact]
+    public void Reset_WhenTreeWasNotChanged_ShouldThrow()
+    {
+        // arrange
+        var tree = new BinaryTree<int>() { 2, 5, 3 };
+        var root = tree.Root;
+        var enumerator = tree.GetEnumerator();
+
+        while (enumerator.MoveNext()) { }
+        tree.Add(-2);
+
+        // act
+        var act = () => enumerator.Reset();
+
+        // assert
+        Assert.Throws<InvalidOperationException>(act);
+    }
+}
